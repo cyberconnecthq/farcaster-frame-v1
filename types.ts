2d9ad1ca8079acc2bfdf00631e4286e5270f1f33
damienWu <damien.wu@cyberconnect.me>
2024-12-13 17:00:41 +0800

    fix: is prd

--- types.ts
+++ types.ts
@@ -1,6 +1,6 @@
 import { base, cyber, cyberTestnet, optimismSepolia } from "viem/chains";
 
-const isPrd = process.env.NEXT_PUBLIC_VERCEL_ENV === "production";
+export const isPrd = false;
 
 export const targetChainId = isPrd ? cyber.id : cyberTestnet.id;
 export const selectedChainId = isPrd ? base.id : optimismSepolia.id;
